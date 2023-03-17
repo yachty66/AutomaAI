c721ce3d61412a67a5a4b31350362bbb1daa3273
Max Hager <maxhager28@gmail.com>
2023-03-17 15:44:57 -0700

    version 1

--- bot.py
+++ bot.py
@@ -1,28 +1,21 @@
-'''
-- [x] parse command
-- [x] create a venv 
-- [ ] send command to gpt
-    - [ ] write the prompt in a way so that the response is expeced. like "Write your response in the following format: Filename: T"
-- [ ] create  
-'''
 import requests
+import re
 import json
+import subprocess
+import config
 
 def parse():
-    #parse the the content from commands.txt 
     with open("Test.txt", "r") as f:
         content = f.read()
     return content
 
 def request():
-    #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
-    #send request to gpt
     command = parse()
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = config.OPENAI_API_KEY
     url = "https://api.openai.com/v1/chat/completions"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + OPENAI_API_KEY # Replace OPENAI_API_KEY with your actual API key
+        "Authorization": "Bearer " + OPENAI_API_KEY
     }
     payload = {
         "model": "gpt-3.5-turbo",
@@ -32,7 +25,36 @@ def request():
     response = requests.post(url, headers=headers, data=json.dumps(payload))
     return response.json()
 
+response = request()
+print(response)
+
+def filename(): 
+    content = response['choices'][0]['message']['content']
+    filename = content.split('\n\n')[1].split(':')[1].strip()
+    with open(filename, "w") as f:
+        f.write("")
+    return filename
+ 
+def script():
+    content = response['choices'][0]['message']['content']
+    pattern = r'Script code:(.*?)Run:'
+    script_code = re.search(pattern, content, re.DOTALL).group(1).strip()
+    with open(filename(), "w") as f:
+        f.write(script_code)
+    return script_code
+
+def run():
+    content = response['choices'][0]['message']['content']
+    output = content.split('Run:')[1].strip()
+    result = subprocess.run(['python', output], capture_output=True, text=True)
+    if result.returncode == 0:
+        print('Script executed successfully.')
+        print('Output:', result.stdout)
+    else:
+        print('Error:', result.stderr)
+    pass
 
 if __name__ == "__main__":
-    #parse()
-    request()
+    filename()
+    script()
+    run()
