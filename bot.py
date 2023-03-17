2ea64b749b4ce1d53febca72688bb941e20c16c3
Max Hager <maxhager28@gmail.com>
2023-03-17 12:53:31 -0700

    add all

---
+++ bot.py
@@ -0,0 +1,8 @@
+'''
+- [ ] parse command
+- [ ] send command to gpt
+    - [ ] write the prompt in a way so that the response is expeced. like "Write your response in the following format: Filename: T"
+- [ ] create  
+
+
+'''
